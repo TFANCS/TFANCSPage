6f5ff8d7a4a2847cce35bd52931baab3210378c7
TFANCS <tsnf.hc.p.mlp2@gmail.com>
2021-06-15 00:55:05 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,3 +2,9 @@ function copyToClipboard(txt) {
     navigator.clipboard.writeText(txt)
     alert("Copied to clipboard");
 }
+
+
+window.onload = function () {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
+}
