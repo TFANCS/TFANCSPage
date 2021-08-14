cbf56a2c7d94d977d799d29ed8cf046d501f9b4f
TFANCS <tsnf.hc.p.mlp2@gmail.com>
2021-08-14 22:15:34 +0900

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -61,9 +61,9 @@ function calcPrice() {
         final_price = base_price + base_price * 0.6 * (char_val - 1);
     } else if (type_val == 2) {
         if (color_val == 2) {
-            base_price = 50;
+            base_price = 55;
         } else if (color_val == 3) {
-            base_price = 65;
+            base_price = 70;
         } else {
             color_val = 2;
             comm_color.options[1].selected = true;
@@ -73,7 +73,7 @@ function calcPrice() {
         final_price = base_price + base_price * 0.7 * (char_val - 1) ** (1.1);
     } else if (type_val == 3) {
         if (color_val == 3) {
-            base_price = 85;
+            base_price = 90;
         } else {
             color_val = 3;
             comm_color.options[2].selected = true;
