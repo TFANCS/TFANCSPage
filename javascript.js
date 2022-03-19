7ca1939248c07067bce409060f2141c512797163
TFANCS <tsnf.hc.p.mlp2@gmail.com>
2022-03-20 00:43:17 +0900

    Updated char size

--- javascript.js
+++ javascript.js
@@ -98,5 +98,41 @@ if (comm_type != null && comm_color != null && comm_char != null && comm_price !
 }
 
 
+if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+} else {
+    console.log('MetaMask is not installed!');
+}
 
+const ethereumButton = document.getElementById('aaaa');
+const sendEthButton = document.getElementById('bbbb');
+
+let accounts = [];
+
+//Sending Ethereum to an address
+sendEthButton.addEventListener('click', () => {
+    ethereum
+        .request({
+            method: 'eth_sendTransaction',
+            params: [
+                {
+                    from: accounts[0],
+                    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                    value: '0x29a2241af62c0000',
+                    gasPrice: '0x09184e72a000',
+                    gas: '0x2710',
+                },
+            ],
+        })
+        .then((txHash) => console.log(txHash))
+        .catch((error) => console.error);
+});
+
+ethereumButton.addEventListener('click', () => {
+    getAccount();
+});
+
+async function getAccount() {
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+}
 
